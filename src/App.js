d40688485260a67aca2e8dd12c45986a63f4a133
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-02 00:26:45 -0400

    Added a background color and border around the gifs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 function App() {
   const [gifsList, setGifs] = useState([]);
   const [searchInput, setSearchInput] = useState("");
+  let input = document.getElementsByClassName("input-fld");
 
   function updateSearch(event){
     setSearchInput(event.target.value);
@@ -15,7 +16,7 @@ function App() {
     fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=J50GUocJ7bqHKPv807g9I10AydOK1DX1`)
     .then(response => response.json())
     .then(data => setGifs(data.data.map(data => <img className='gifs' src={data.images.original.url}></img>)));
-
+    setSearchInput("");
     
   }// End of the fetchGifs function
 
@@ -23,12 +24,15 @@ function App() {
     
    <>
 
-   <h1>Gifs on Gifs</h1>
-   <form onSubmit={fetchGifs}>
-   <input onChange={updateSearch} type="text"/>
+   <h1 className='gifsongifs'>Gifs on Gifs</h1>
+   <form className='searchField' onSubmit={fetchGifs}>
+   <input className='input-fld' onChange={updateSearch} type="text" value={searchInput}/>
    <button>Search</button>
    </form>
-    {gifsList}
+   <div className='gifContainer'>
+   {gifsList}
+   </div>
+    
    </>
   );
 }
