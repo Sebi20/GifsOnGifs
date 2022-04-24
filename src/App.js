aa792b9ef74e48a1b72213dfc758969a741bb75f
Sebastian <sebastian.stephenson1@gmail.com>
2022-04-24 11:11:17 -0400

    Rendering the gifs with a function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import axios from 'axios'
 
 function App() {
   const [gifsList, setGifs] = useState([]);
@@ -16,11 +17,11 @@ function App() {
   const fetchGifs = async (event) =>{
     setGifs([]);
     event.preventDefault();
-    const data = await fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=J50GUocJ7bqHKPv807g9I10AydOK1DX1`);
-    const gifs = await data.json();
-    console.log(gifs);
-    setGifs(gifs.data.map(data => <img className='gifs' src={data.images.original.url}></img>));
-    setTerm(`The current search is for: ${searchInput}`);
+
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=J50GUocJ7bqHKPv807g9I10AydOK1DX1`);
+    console.log(res.data.data);
+    setGifs(res.data.data);
+    setTerm(searchInput); 
     setSearchInput("");
   }
 
@@ -28,22 +29,33 @@ function App() {
     fetchGifs(event);
   }, []);
 
-   
+  const gifs = gifsList.map(gifs =>{
+
+    return (
+      <div>
+        <img className='gifs' src={gifs.images.original.url}></img>
+
+      </div>
+    )
+
+  })
+
+
 
   return (
     
    <>
+    <h1 className='gifsongifs'>Gifs on Gifs</h1>
+    <form className='searchField' onSubmit={fetchGifs}>
+      <input className='input-fld' onChange={updateSearch} placeholder="Search for gif..." type="text" value={searchInput}/>
+      <button>Search</button>
 
-   <h1 className='gifsongifs'>Gifs on Gifs</h1>
-   <form className='searchField' onSubmit={fetchGifs}>
-   <input className='input-fld' onChange={updateSearch} placeholder="Search for gif..." type="text" value={searchInput}/>
-   <button>Search</button>
-   </form>
+    </form>
 
-   <h3 className='userSearchTerm'>{term}</h3>
-   <div className='gifContainer'>
-   {gifsList}
-   </div>
+    <h3 className='userSearchTerm'>The current search is for: <span className='searchTerm'>{term}</span></h3>
+    <div className='gifContainer'>
+    {gifs}
+    </div>
     
    </>
   );
