831609c7c2143c178a174884b392b9dd8928f586
Sebastian <sebastian.stephenson1@gmail.com>
2022-05-04 12:43:49 -0400

    Made the title first letter uppercase

--- src/Gif.js
+++ src/Gif.js
@@ -4,17 +4,27 @@ import './Gifs.css'
 
 function Gif(prop){
     const location = useLocation();
+
+    //Gets the first letter of the title and then replacing the first letter with an uppercase first letter
+    let firstLetter = location.state.title.substring(0, 1);
+    let title = (location.state.title.replace(firstLetter, firstLetter.toUpperCase()));
+
+    //Gets the rating and sets it to uppercase
+    let rating = location.state.rating.toUpperCase();
     
     
     return (
         <>
 
             <Link className="linkToHome" to={"/GifsOnGifs"} underline="none"><h1 className='gifsongifs'>Gifs on Gifs</h1></Link>
+
+            {/* <h4 className='back-btn'>{"<-"}Back</h4> */}
+
             <div className='content'>
                 <img className='gif' src={location.state.img}></img>
                 <div className='underGif'>
-                    <p>{location.state.title}</p>
-                    <p>Rated: {location.state.rating}</p>
+                    <p className='title'>{title}</p>
+                    <p className='rating'>Rated: {rating}</p>
                     <a href='public/images/downloadIcon.png'></a>
                 </div>
             </div>
