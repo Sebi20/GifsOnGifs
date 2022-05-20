40b097af06aa2dbacb06f1840ba69499f8f96868
Sebastian <sebastian.stephenson1@gmail.com>
2022-05-20 12:34:45 -0400

    Added gif to local storage, but the refresh issue still exists

--- src/Gif.js
+++ src/Gif.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {useLocation, Link} from "react-router-dom"
+import {useState} from 'react'
 import './Gifs.css'
 import downloadIcon from './images/downloadIcon.png'
 
 
 function Gif(prop){
     const location = useLocation();
+    localStorage.setItem("gif", location.state.img);
+
+    const[gif, setGif] = useState(localStorage.getItem("gif"));
+
+
 
     //Gets the first letter of the title and then replacing the first letter with an uppercase first letter
     let firstLetter = location.state.title.substring(0, 1);
@@ -14,21 +20,20 @@ function Gif(prop){
     //Gets the rating and sets it to uppercase
     let rating = location.state.rating.toUpperCase();
 
-    let imageURL;
-
     const downloadSelected = async () => {
-       
-         let image = await fetch(location.state.img)
-         let blob = await image.blob()
-         imageURL = URL.createObjectURL(blob)
+        let temp = document.createElement("a")
     
+        document.body.appendChild(temp)
 
-        console.log(imageURL);
-
-
+        let image = await fetch(location.state.img)
+        let blob = await image.blob()
+        let imageURL = URL.createObjectURL(blob)
+        temp.setAttribute("href",imageURL)
+        console.log(imageURL)
+        temp.setAttribute("download",`${title}`)
+        temp.click()
+        temp.remove()
       }
-
-      downloadSelected();
 
 
 
@@ -39,11 +44,11 @@ function Gif(prop){
             <Link className="linkToHome" to={"/GifsOnGifs"} underline="none"><h1 className='gifsongifs'>Gifs on Gifs</h1></Link>
 
             <div className='content'>
-                <img className='gif' src={location.state.img} alt=""></img>
+                <img className='gif' src={gif} alt=""></img>
                 <div className='underGif'>
                     <p className='title'>{title}</p>
                     <p className='rating'>Rated: {rating}</p>
-                    <a href={imageURL} download={title}><img className='download' src={downloadIcon} ></img></a>
+                    <img className='download' onClick={downloadSelected} src={downloadIcon} ></img>
                 </div>
             </div>
         </>
