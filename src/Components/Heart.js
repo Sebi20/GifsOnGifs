4768995950f03cddbeaabb940facd79be589d513
Sebastian <sebastian.stephenson1@gmail.com>
2022-06-24 19:01:07 -0400

    Added a heart component that tracks what gifs will be the user's favorites

---
+++ src/Components/Heart.js
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+import '../CSS/Heart.css'
+
+function Heart(){
+    const[heart, setHeart] = useState("");
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Heart;
